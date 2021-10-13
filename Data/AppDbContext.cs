5ceb4713a9bbe7a750e099f4798cf73c58a282c0
Juan Mendoza <a2020111209@estudiantes.upsa.edu.bo>
2021-10-13 02:06:23 -0400

    AppDbContext creado

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CancioneWebAPI1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancioneWebAPI1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Cancion> Cancion { get; set; }
+    }
+}
